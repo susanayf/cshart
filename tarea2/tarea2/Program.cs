b3d4c6f7deda7aae9b8b4db0ade0c0c1a827cad8
Susana YF <susanayuvinca@gmail.com>
2024-06-24 17:45:46 -0300

    tarea2
    ejercicio de sumas

---
+++ tarea2/tarea2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+int num2;
+int resul;
+Console.WriteLine("ingrese el primer numero a sumar");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ingrese el segundo numero a sumar");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+resul = num1 + num2;
+Console.WriteLine("el resultado dde la suma es: " + resul);
+;
