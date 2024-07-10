0bef7fb1a4acebe705e4dcc175e866a946b00d7c
Susana YF <susanayuvinca@gmail.com>
2024-07-10 17:00:16 -0300

    tarea3

---
+++ tarea3/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Timers;
+
+string comision;
+int nota = 0;
+
+Console.WriteLine("ingrese la comision A, B o C");
+comision= Console.ReadLine();
+
+switch (comision) 
+{
+    case "A":
+    Console.WriteLine("Turno mañana");
+        break;
+
+        case "B":
+        Console.WriteLine("turno tarde");
+        break;
+
+        case "C":
+        Console.WriteLine("turno noche");
+        break;
+
+        default:
+        Console.WriteLine("el alumno no tiene turno fijado");
+        break;
+}
+    Console.WriteLine("ingrese la nota del alumno de 0 a 10");
+    nota=Convert.ToInt32(Console.ReadLine());
+
+if (nota < 4)
+{
+    Console.WriteLine("el alumno recursa la materia");
+}
+if (nota >=4 && nota <= 7)
+{
+    Console.WriteLine("el alumno aprobo la cursada");
+}
+else if (nota > 7)
+{
+    Console.WriteLine("el alumno promociona la materia");
+}
