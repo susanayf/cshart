859bda1b1d3abaa94f522fd9975228a439940897
Susana YF <susanayuvinca@gmail.com>
2024-07-11 13:16:57 -0300

    tarea6

---
+++ tarea6/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//string cadenanula = null;
+//string cadenavacia = string.Empty;
+//string cadenaconsaludo = "hola";
+
+////Console.WriteLine(cadenanula.Length); este se queda comentada porque es null y por ende lanza error
+//Console.WriteLine(cadenavacia.Length);
+//Console.WriteLine(cadenaconsaludo.Length);
+
+
+//string cadena="LOS ToupPER Y Tolower";
+
+//Console.WriteLine("la cadena esta en minuscula: " + cadena.ToLower());
+//Console.WriteLine("la cadena esta en mayuscula: "+ cadena.ToUpper());
+//Console.ReadKey();
+
+int entero = 1234;
+double numeroconDecimales = 3.1415;
+bool booleana = true;
+
+Console.WriteLine(entero.ToString()  + " " + numeroconDecimales.ToString()  + " " + booleana.ToString());
